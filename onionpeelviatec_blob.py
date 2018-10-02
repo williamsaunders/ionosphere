9a8ed101f8f45d60a25ef0e55d70d66f094cf951
William Saunders <william.saunders01@gmail.com>
2018-10-02 15:23:54 -0400

    Working basically.  Bad organization but mostly temp. at this point. Major re-writes and re-organization to come in following weeks.

--- onionpeelviatec_blob.py
+++ onionpeelviatec_blob.py
@@ -14,7 +14,6 @@ plt.rcParams['font.size']=14
 matplotlib.rcParams['xtick.direction'] = 'out'
 matplotlib.rcParams['ytick.direction'] = 'out'
 
-#
 rpkm = 3400. # Rmars in km
 r0km = 120. + rpkm # altitude of electron density peak at subsolar point in km in radial distance
 n0cm3 = 2e5 # peak electron density at the subsolar point, cm-3
@@ -47,24 +46,47 @@ theta2ddeg = theta2rad * np.pi/180.
 dummyx2d = (rarr2dkm - r0km) / dsh0km
 nelec2dcm3 = n0cm3 * np.exp(1 - dummyx2d - np.exp(-dummyx2d))
 
+# add blob of plasma modeled as a gaussian distribution
+sigma = .05*nx
+#mu = (np.random.choice(range(nx)), np.random.choice(ny))
+mu = (int(nx/2.),int(ny/10.))
+xg = (1/np.sqrt(2*np.pi)) * np.exp(-.5*((xarr1dkm-xarr1dkm[mu[0]])/sigma)**2)
+yg = (1/np.sqrt(2*np.pi)) * np.exp(-.5*((yarr1dkm-yarr1dkm[mu[1]])/sigma)**2)
+Xg, Yg= np.meshgrid(yg,xg)
+peak = nelec2dcm3[mu[0],mu[1]]*1e5
+nelec2dcm3_g = n0cm3*Xg*Yg*peak
+
+nelec2dcm3_orig = copy.copy(nelec2dcm3)
+nelec2dcm3 = nelec2dcm3 + nelec2dcm3_g
+
+plt.figure(figsize=(12,7))
+X = xarr2dkm
+Y = yarr2dkm
+Z = np.log(nelec2dcm3_g)
+CS = plt.contourf(X, Y, Z, [-400,-350,-300,-250,-200,-150,-100,-50,0,50,100], cmap=cm.PuBu_r, vmin=-400, vmax=100)
+cbar = plt.colorbar(CS,ticks=[-400,-350,-300,-250,-200,-150,-100,-50,0,50,100])
+cbar.ax.set_ylabel(r'log electron density [cm$^{-3}$]')
+plt.xlabel('X position [km]', fontsize=20)
+plt.ylabel('Y position [km]', fontsize=20)
+plt.gcf().subplots_adjust(bottom=0.15)
+plt.grid()
+plt.tight_layout()
+plt.show()
+#plt.savefig('/Users/saunders/Documents/planet_research/contour_sym.png', bbox_inches='tight')
+
+
 # get rid of 0 density values so we can take the log
 x0, y0 = np.where(nelec2dcm3==0)
 nelec2dcm3_0 = copy.copy(nelec2dcm3) #make a copy that we can mess with
 nelec2dcm3_0[x0,y0] = 1e-300
 
 # make contour plot of electorn density
-#plt.contourf(yarr1dkm, xarr1dkm, nelec2dcm3, np.sort(histedges_equalN(np.concatenate(nelec2dcm3),200)))
-#plt.colorbar()
-#plt.show()
-
-
-# make color contour plot for retention
 plt.figure(figsize=(12,7))
 X = xarr2dkm
 Y = yarr2dkm
 Z = np.log(nelec2dcm3_0)
-CS = plt.contourf(X, Y, Z, cmap=cm.PuBu_r)
-cbar = plt.colorbar(CS)
+CS = plt.contourf(X, Y, Z, [-400,-350,-300,-250,-200,-150,-100,-50,0,50,100], cmap=cm.PuBu_r, vmin=-400, vmax=100)
+cbar = plt.colorbar(CS,ticks=[-400,-350,-300,-250,-200,-150,-100,-50,0,50,100])
 cbar.ax.set_ylabel(r'log electron density [cm$^{-3}$]')
 
 # formatting and labeling
@@ -74,11 +96,9 @@ plt.gcf().subplots_adjust(bottom=0.15)
 plt.grid()
 #plt.legend(loc='lower left')
 #plt.gca().add_artist(legend1)
-#plt.title(r'Disk Evolution in $\alpha$-$\dot{M_{pe}}$ Space')
 plt.tight_layout()
+plt.savefig('/Users/saunders/Documents/planet_research/contour_blob.png', bbox_inches='tight', dpi=100)
 plt.show()
-plt.savefig('/Users/saunders/Documents/planet_research/contour_sym.png', bbox_inches='tight')
-
 
 
 # unit conversions 
@@ -124,10 +144,58 @@ while i < len(newrevdxdr):
     invnelec1dm3.append(stuff/np.pi)
     i += 1
 
+# DO EVERYTHING AGAIN FOR THE ORIGINAL  (NO BLOB)
+
+# unit conversions 
+nelec2dm3 = nelec2dcm3_orig * 1e6
+xarr1dm = xarr1dkm * 1e3
+yarr1dm = yarr1dkm * 1e3
+
+dl = xarr1dm[1] - xarr1dm[0]
+tec1dm2 = np.sum(nelec2dm3, axis=0) * dl
+
+# change variable names 
+newbigx = np.array(tec1dm2)
+xa = np.array(yarr1dm)
+
+# perform derivative: dOmega/dX
+newdxdr = (np.roll(newbigx,-1) - np.roll(newbigx,1)) / (np.roll(xa,-1) - np.roll(xa,1))
+newdxdr[0] = 2.*newdxdr[2] - newdxdr[1]
+newdxdr[-1] = 2.*newdxdr[-3] - newdxdr[-2]
+
+# some math checks 
+pp = np.argsort(xa) # make sure order is correct becuase sometimes radio occultations \ 
+                    # come out of order becuase of refraction or direction of transit 
+reva2 = xa[pp] # ordered impact parameter, m (closest approach distance)
+thisrkm = reva2 / 1e3 # ordered impact parameter, km
+newrevdxdr = newdxdr[pp] # ordered spatial derivative of bigx, m-3
+
+# set first element of newrevdxdr to 0 to avoid problems with numerical integration
+newrevdxdr[-1] = 0.
+
+# electrondensity is the local election density m-3, from corrected data
+# perform integration
+
+
+invnelec1dm3_orig = []
+i = 0
+while i < len(newrevdxdr):
+    stuff = 0.
+    j = i
+    while j < len(newrevdxdr) - 1:
+        stuff = stuff + .5 * ( np.log(reva2[j]/reva2[i] + np.sqrt((reva2[j]/reva2[i])**2. - 1.)) + \
+                np.log(reva2[j+1]/reva2[i] + np.sqrt((reva2[j+1]/reva2[i])**2. - 1.)) ) \
+                * (newrevdxdr[j+1] - newrevdxdr[j])
+        j += 1
+    invnelec1dm3_orig.append(stuff/np.pi)
+    i += 1
+
 invnelec1dm3 = np.array(invnelec1dm3)
-plt.semilogx(invnelec1dm3, yarr1dkm-rpkm, linewidth=3)
-plt.semilogx(nelec2dm3[nc,:], yarr1dkm-rpkm)
-plt.xlim(1e-2,1e12)
-plt.ylim(0,400)
+plt.semilogx(invnelec1dm3, yarr1dkm-rpkm, linewidth=3, color='r', label='blob integrated electron column density')
+plt.semilogx(invnelec1dm3_orig, yarr1dkm-rpkm, linewidth=1, color='b', label='original integrated electron column density')
+#plt.semilogx(nelec2dm3[nc,:], yarr1dkm-rpkm, color='b', label='predicted electron column density')
+plt.xlim(1e-10,1e12)
+plt.ylim(0,500)
+plt.legend()
+plt.savefig('/Users/saunders/Documents/planet_research/electrondensity_comparison.png', bbox_inches='tight')
 plt.show()
-plt.savefig('/Users/saunders/Documents/planet_research/coldensity_sym.png', bbox_inches='tight')
--- onionpeelviatec_half.py
+++ onionpeelviatec_half.py
@@ -14,23 +14,15 @@ plt.rcParams['font.size']=14
 matplotlib.rcParams['xtick.direction'] = 'out'
 matplotlib.rcParams['ytick.direction'] = 'out'
 
-
-def histedges_equalN(x, nbin):
-    npt = len(x)
-    return np.interp(np.linspace(0, npt, nbin + 1),
-                     np.arange(npt),
-                     np.sort(x))
-
-
 rpkm = 3400. # Rmars in km
 r0km = 120. + rpkm # altitude of electron density peak at subsolar point in km in radial distance
 n0cm3 = 2e5 # peak electron density at the subsolar point, cm-3
 dsh0km = 10. # density scale height, km
 
 # create grid to keep track of electron density
-nx = 20001
+nx = 2001
 nc = round((nx-1.)/2)
-ny = 10001
+ny = 1001
 
 xarr1dkm = (2*np.arange(nx)/(nx-1) - 1) * rpkm 
 yarr1dkm = (np.arange(ny)/(ny-1) + 1) * rpkm
@@ -53,6 +45,10 @@ theta2ddeg = theta2rad * np.pi/180.
 # define spherical ionosphere 
 dummyx2d = (rarr2dkm - r0km) / dsh0km
 nelec2dcm3 = n0cm3 * np.exp(1 - dummyx2d - np.exp(-dummyx2d))
+nelec2dcm3_orig = copy.copy(nelec2dcm3)
+
+# make right half higher density 
+nelec2dcm3[int(nx/2):,:] = nelec2dcm3_orig[int(nx/2):,:]*1e5
 
 # get rid of 0 density values so we can take the log
 x0, y0 = np.where(nelec2dcm3==0)
@@ -60,12 +56,6 @@ nelec2dcm3_0 = copy.copy(nelec2dcm3) #make a copy that we can mess with
 nelec2dcm3_0[x0,y0] = 1e-300
 
 # make contour plot of electorn density
-#plt.contourf(yarr1dkm, xarr1dkm, nelec2dcm3, np.sort(histedges_equalN(np.concatenate(nelec2dcm3),200)))
-#plt.colorbar()
-#plt.show()
-
-
-# make color contour plot for retention
 plt.figure(figsize=(12,7))
 X = xarr2dkm
 Y = yarr2dkm
@@ -81,12 +71,9 @@ plt.gcf().subplots_adjust(bottom=0.15)
 plt.grid()
 #plt.legend(loc='lower left')
 #plt.gca().add_artist(legend1)
-#plt.title(r'Disk Evolution in $\alpha$-$\dot{M_{pe}}$ Space')
 plt.tight_layout()
-plt.savefig('/Users/saunders/Documents/planet_research/contour_sym-large.png', bbox_inches='tight')
+plt.savefig('/Users/saunders/Documents/planet_research/contour_half2.png', bbox_inches='tight')
 plt.show()
-
-
 
 
 # unit conversions 
@@ -123,7 +110,6 @@ invnelec1dm3 = []
 i = 0
 while i < len(newrevdxdr):
     stuff = 0.
-    print(i, '/', len(newrevdxdr))
     j = i
     while j < len(newrevdxdr) - 1:
         stuff = stuff + .5 * ( np.log(reva2[j]/reva2[i] + np.sqrt((reva2[j]/reva2[i])**2. - 1.)) + \
@@ -133,11 +119,58 @@ while i < len(newrevdxdr):
     invnelec1dm3.append(stuff/np.pi)
     i += 1
 
-invnelec1dm3 = np.array(invnelec1dm3)
-plt.semilogx(invnelec1dm3, yarr1dkm-rpkm, linewidth=3)
-plt.semilogx(nelec2dm3[nc,:], yarr1dkm-rpkm)
-plt.xlim(1e-2,1e12)
-plt.ylim(0,400)
-plt.savefig('/Users/saunders/Documents/planet_research/elec_sym-large.png', bbox_inches='tight')
-plt.show()
+# DO EVERYTHING AGAIN FOR THE ORIGINAL  (NO BLOB)
 
+# unit conversions 
+nelec2dm3 = nelec2dcm3_orig * 1e6
+xarr1dm = xarr1dkm * 1e3
+yarr1dm = yarr1dkm * 1e3
+
+dl = xarr1dm[1] - xarr1dm[0]
+tec1dm2 = np.sum(nelec2dm3, axis=0) * dl
+
+# change variable names 
+newbigx = np.array(tec1dm2)
+xa = np.array(yarr1dm)
+
+# perform derivative: dOmega/dX
+newdxdr = (np.roll(newbigx,-1) - np.roll(newbigx,1)) / (np.roll(xa,-1) - np.roll(xa,1))
+newdxdr[0] = 2.*newdxdr[2] - newdxdr[1]
+newdxdr[-1] = 2.*newdxdr[-3] - newdxdr[-2]
+
+# some math checks 
+pp = np.argsort(xa) # make sure order is correct becuase sometimes radio occultations \ 
+                    # come out of order becuase of refraction or direction of transit 
+reva2 = xa[pp] # ordered impact parameter, m (closest approach distance)
+thisrkm = reva2 / 1e3 # ordered impact parameter, km
+newrevdxdr = newdxdr[pp] # ordered spatial derivative of bigx, m-3
+
+# set first element of newrevdxdr to 0 to avoid problems with numerical integration
+newrevdxdr[-1] = 0.
+
+# electrondensity is the local election density m-3, from corrected data
+# perform integration
+
+
+invnelec1dm3_orig = []
+i = 0
+while i < len(newrevdxdr):
+    stuff = 0.
+    j = i
+    while j < len(newrevdxdr) - 1:
+        stuff = stuff + .5 * ( np.log(reva2[j]/reva2[i] + np.sqrt((reva2[j]/reva2[i])**2. - 1.)) + \
+                np.log(reva2[j+1]/reva2[i] + np.sqrt((reva2[j+1]/reva2[i])**2. - 1.)) ) \
+                * (newrevdxdr[j+1] - newrevdxdr[j])
+        j += 1
+    invnelec1dm3_orig.append(stuff/np.pi)
+    i += 1
+
+invnelec1dm3 = np.array(invnelec1dm3)
+plt.semilogx(invnelec1dm3, yarr1dkm-rpkm, linewidth=3, color='r', label='blob integrated electron column density')
+plt.semilogx(invnelec1dm3_orig, yarr1dkm-rpkm, linewidth=1, color='b', label='original integrated electron column density')
+#plt.semilogx(nelec2dm3[nc,:], yarr1dkm-rpkm, color='b', label='predicted electron column density')
+plt.xlim(1e-10,1e12)
+plt.ylim(0,500)
+plt.legend()
+plt.savefig('/Users/saunders/Documents/planet_research/elec_half2.png', bbox_inches='tight')
+plt.show()
